1a965c1689fcb43360ec63196a7940863d9839b0
Blake Ingenthron <blakeingenthron@gmail.com>
2019-11-05 14:12:13 -0600

    intial commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,21 +1,24 @@
-const request = require('request')
+const request = require("request");
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+	const url =
+		"https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+		address +
+		".json?access_token=pk.eyJ1IjoiYmxha2VpbmciLCJhIjoiY2p5cWZ6N3MzMDFmODNqbXMybnUxMTN0bCJ9.6ieJU1E5IpbSPS1m_IkUGw";
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
-        } else {
-            callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
-            })
-        }
-    })
-}
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback("Unable to connect to location services!", undefined);
+		} else if (body.features.length === 0) {
+			callback("Unable to find location. Try another search.", undefined);
+		} else {
+			callback(undefined, {
+				latitude: body.features[0].center[1],
+				longitude: body.features[0].center[0],
+				location: body.features[0].place_name
+			});
+		}
+	});
+};
 
-module.exports = geocode
+module.exports = geocode;
